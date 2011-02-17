3383882c17e0515c59b5a03627ea1728b561232c
Dan Harper <intouch@danharper.me>
2011-02-17 18:23:07 +0000

    Some progress

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 $(function() {
 
-	var html, logo, slogan, nav;
+	var html, logo, slogan, nav, postheading, postdate;
 
 	html = document.documentElement;
 	logo = document.getElementsByTagName("h1")[0];
 	slogan = $("hgroup h2")[0];
 	nav = $("header nav")[0];
+	postheading = $("#singlepost h2");
+	postdate = $("#singlepost header p");
 
+	// Change class on html tag from 'no-js' to 'js'
 	html.className = html.className.replace(/\bno-js\b/,'') + 'js';
 
+	// Show slogan when hover on logo
 	$(logo).hover(
 		function () {
-			$(nav).hide("fast");
-			$(slogan).show("fast");
+			$(nav).stop(true,true).hide("fast");
+			$(slogan).stop(true,true).show("fast");
 		},
 		function () {
 			$(slogan).hide("fast");
 			$(nav).show("fast");
+		}
+	);
+
+	// Show post date when hover on title
+	$(postheading).hover(
+		function () {
+			$(postdate).stop(true,true).fadeIn(300);
+		},
+		function () {
+			$(postdate).stop(true,true).fadeOut(300);
 		}
 	);
 
