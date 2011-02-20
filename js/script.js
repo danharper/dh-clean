bdd2f89256c327f0d7a1d0936197f714190763e5
Dan Harper <intouch@danharper.me>
2011-02-20 19:04:59 +0000

    Enhancing home page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	var html, logo, slogan, nav, postheading, postdate;
+	var html, logo, slogan, nav, postheading, postdate, homenav, homeh1;
 
 	html = document.documentElement;
 	logo = document.getElementsByTagName("h1")[0];
@@ -8,6 +8,8 @@ $(function() {
 	nav = $("header nav")[0];
 	postheading = $("#singlepost h2");
 	postdate = $("#singlepost header p");
+	homenav = $("#home li a");
+	homeh1 = $("#home h1");
 
 	// Change class on html tag from 'no-js' to 'js'
 	html.className = html.className.replace(/\bno-js\b/,'') + 'js';
@@ -31,6 +33,24 @@ $(function() {
 		},
 		function () {
 			$(postdate).stop(true,true).fadeOut(300);
+		}
+	);
+
+	// Change homepage logo when hover over nav items
+	$(homenav).hover(
+		function () {
+			if (this === homenav[0]) {
+				homeh1.css('backgroundPosition', '-210px');
+			}
+			else if (this === homenav[1]) {
+				homeh1.css('backgroundPosition', '-420px');
+			}
+			else {
+				homeh1.css('backgroundPosition', '-630px');
+			}
+		},
+		function () {
+			homeh1.css('backgroundPosition', '0');
 		}
 	);
 
